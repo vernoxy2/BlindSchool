a4fba09da4c0f3fa61c6b7d7e899a2202f93aa15
vernoxy <vernoxy3@gmail.com>
2025-11-18 14:28:22 +0530

    code push

--- src/pages/AbouUS/About.jsx
+++ src/pages/AbouUS/About.jsx
@@ -3,7 +3,7 @@ import Head from "./AboutSections/Head";
 import Committed from "./AboutSections/Committed";
 import OurPhilosophy from "./AboutSections/OurPhilosophy";
 import OurJourney from "./AboutSections/OurJourney ";
-import Expanding from "./AboutSections/Expanding.JSX";
+import Expanding from "./AboutSections/Expanding";
 import OurMissionVision from "./AboutSections/OurMissionVision";
 import Certificate from "./AboutSections/Certificate";
 import Help from "../Homepage/HomePageSections/Help";
