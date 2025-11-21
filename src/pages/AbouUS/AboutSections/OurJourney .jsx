ed1e2a471959e787160e22a9244ea89f966749e4
vernoxy <vernoxy3@gmail.com>
2025-11-21 09:31:36 +0530

    fix: AboutUs section develop

--- src/pages/AbouUS/AboutSections/OurJourney .jsx
+++ src/pages/AbouUS/AboutSections/OurJourney .jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const OurJourney  = () => {
+const OurJourney = () => {
   return (
-    <div className='text-center'>
-      OurJourney
-    </div>
-  )
-}
+    <section className="text-center flex flex-col items-center px-4 py-12">
+      <h1 className="text-4xl font-bold text-primary mb-4">
+        Our Journey of Growth <br /> and Impact
+      </h1>
 
-export default OurJourney 
+      <p className="max-w-xl leading-relaxed">
+        From a small beginning to a thriving community of hope every decade at
+        NAB Valsad has been a step toward empowerment, inclusion, and
+        independence for the differently-abled.
+      </p>
+    </section>
+  );
+};
+
+export default OurJourney;
